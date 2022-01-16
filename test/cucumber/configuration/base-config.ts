1347f488fca2a1d170a03de11e0f128e5d7be9e1
Yon <yon.cuadrado@gmail.com>
2022-01-16 21:32:06 +0100

    packages udpated

--- test/cucumber/configuration/base-config.ts
+++ test/cucumber/configuration/base-config.ts
@@ -1,141 +1,141 @@
 import 'reflect-metadata';
-import CucumberJsJsonReporter from 'wdio-cucumberjs-json-reporter-with-typescript';
 import { CustomCommands } from '../custom-commands/custom-commands';
 import { registerPagesInContainer } from '../container/container';
-import {generateReport} from 'cucumber-html-report-generator'
 import path from 'path';
+import { CucumberHtmlReporter } from 'wdio-reporter-html'
+
+const jsonFolder = '.tmp/cucumberjs-json/';
 
 const config: WebdriverIO.Config = {
-  autoCompileOpts: {
-    autoCompile: true,
-    // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
-    // for all available options
-    tsNodeOpts: {
-      transpileOnly: true,
-      project: 'tsconfig.json'
-    },
-    // tsConfigPathsOpts:{
-    //   baseUrl: './',
-    //   paths: { '@models': [ 'src/lib/models/models.ts' ], '@common-functions': [ 'src/lib/common-functions/common-functions.ts' ] }
-    // }
-  },
-  //
-  // ==================
-  // Specify Test Files
-  // ==================
-  // Define which test specs should run. The pattern is relative to the directory
-  // From which `wdio` was called. Notice that, if you are calling `wdio` from an
-  // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
-  // Directory is where your package.json resides, so `wdio` will be called from there.
-  //
-  maxInstances: 1,
-  capabilities: [],
-  //
-  // ===================
-  // Test Configurations
-  // ===================
-  // Define all options that are relevant for the WebdriverIO instance here
-  //
-  // By default WebdriverIO commands are executed in a synchronous way using
-  // The wdio-sync package. If you still want to run your tests in an async way
-  // E.g. using promises you can set the sync option to false.
-  // Sync: true,
-
-  logLevel: 'error',
-  bail: 0,
-  waitforTimeout: 15000,
-  waitforInterval: 1000,
-  connectionRetryTimeout: 90000,
-  connectionRetryCount: 3,
-  framework: 'cucumber',
-  reporters: [
-    [
-      CucumberJsJsonReporter,
-      {
-        jsonFolder: '.tmp/cucumberjs-json/',
-        language: 'en',
+      autoCompileOpts: {
+            autoCompile: true,
+            // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
+            // for all available options
+            tsNodeOpts: {
+                  transpileOnly: true,
+                  project: 'tsconfig.json'
+            },
+            // tsConfigPathsOpts:{
+            //   baseUrl: './',
+            //   paths: { '@models': [ 'src/lib/models/models.ts' ], '@common-functions': [ 'src/lib/common-functions/common-functions.ts' ] }
+            // }
       },
-    ],
-    'dot',
-    'spec',
-  ],
+      //
+      // ==================
+      // Specify Test Files
+      // ==================
+      // Define which test specs should run. The pattern is relative to the directory
+      // From which `wdio` was called. Notice that, if you are calling `wdio` from an
+      // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
+      // Directory is where your package.json resides, so `wdio` will be called from there.
+      //
+      maxInstances: 1,
+      capabilities: [],
+      //
+      // ===================
+      // Test Configurations
+      // ===================
+      // Define all options that are relevant for the WebdriverIO instance here
+      //
+      // By default WebdriverIO commands are executed in a synchronous way using
+      // The wdio-sync package. If you still want to run your tests in an async way
+      // E.g. using promises you can set the sync option to false.
+      // Sync: true,
 
-  //
-  // =====
-  // Hooks
-  // =====
-  // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
-  // It and to build services around it. You can either apply a single function or an array of
-  // Methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
-  // Resolved to continue.
+      logLevel: 'error',
+      bail: 0,
+      waitforTimeout: 15000,
+      waitforInterval: 1000,
+      connectionRetryTimeout: 90000,
+      connectionRetryCount: 3,
+      framework: 'cucumber',
+      reporters: [
+            [
+                  CucumberHtmlReporter, {}
+            ],
+            'dot',
+            'spec',
+      ],
 
-  before: async (): Promise<void> => {
-    try {
-      await browser.maximizeWindow();
-    } catch ( error: unknown ) {
-      console.log( ( <Error>error ).message );
-    }
-    new CustomCommands().createCutomCommands();
-    registerPagesInContainer();
-  },
+      //
+      // =====
+      // Hooks
+      // =====
+      // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+      // It and to build services around it. You can either apply a single function or an array of
+      // Methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+      // Resolved to continue.
 
-  afterStep: async ( ): Promise<void> => {
-    CucumberJsJsonReporter.attach( await browser.takeScreenshot(), 'image/png' );
-  },
+      before: async (): Promise<void> => {
+            try {
+                  await browser.maximizeWindow();
+            } catch ( error: unknown ) {
+                  console.log( ( <Error>error ).message );
+            }
+            new CustomCommands().createCutomCommands();
+            registerPagesInContainer();
+      },
 
-  after: async (): Promise<void> => {
-      await generateReport.generate({
-            jsonDir: `${path.resolve( './' )}/.tmp/cucumberjs-json/`,
-            openReportInBrowser: true
-      })
-  },
+      afterStep: async (): Promise<void> => {
+            CucumberHtmlReporter.attach( await browser.takeScreenshot(), 'image/png' );
+      },
 
-  services: [
-    [
-      'selenium-standalone',
-      {
-        logPath: './.tmp/',
-        installArgs: {
-          version: '4.0.0-beta-4',
-          drivers: {
-            chrome: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'https://chromedriver.storage.googleapis.com',
-            },
-            firefox: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'https://github.com/mozilla/geckodriver/releases/download',
-            },
-            chromiumedge: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'http://selenium-release.storage.googleapis.com/',
-            },
-          },
-        },
-        args: {
-          version: '4.0.0-beta-4',
-          drivers: {
-            chrome: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'https://chromedriver.storage.googleapis.com',
-            },
-            firefox: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'https://github.com/mozilla/geckodriver/releases/download',
-            },
-            chromiumedge: {
-              version: 'latest',
-              arch: process.arch,
-              baseURL: 'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver',
-            },
-          },
-        },
-      } ] ],
+      onComplete: async (): Promise<void> => {
+            await CucumberHtmlReporter.generateHtmlReport({
+                  jsonDir: `${path.resolve( './' )}/.tmp/json/`,
+                  saveCollectedJSON: true,
+                  saveEnrichedJSON: true,
+                  mongooseServerUrl:"http://localhost:3000",
+                  saveReportInMongoDb: true
+            })
+      },
+
+      services: [
+            [
+                  'selenium-standalone',
+                  {
+                        logPath: './.tmp/',
+                        installArgs: {
+                              version: '4.0.0',
+                              drivers: {
+                                    chrome: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://chromedriver.storage.googleapis.com',
+                                    },
+                                    firefox: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://github.com/mozilla/geckodriver/releases/download',
+                                    },
+                                    chromiumedge: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://msedgedriver.azureedge.net/',
+                                    },
+                              },
+                        },
+                        args: {
+                              version: '4.0.0',
+                              drivers: {
+                                    chrome: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://chromedriver.storage.googleapis.com',
+                                    },
+                                    firefox: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://github.com/mozilla/geckodriver/releases/download',
+                                    },
+                                    chromiumedge: {
+                                          version: 'latest',
+                                          arch: process.arch,
+                                          baseURL: 'https://msedgedriver.azureedge.net/',
+                                    },
+                              },
+                        },
+                  }]],
 };
 
 export { config };
